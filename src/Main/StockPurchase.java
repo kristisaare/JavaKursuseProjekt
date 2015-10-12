445d366d062aef219a690b91e296bcd46a047d20
kristisaare <kristisaare@gmail.com>
2015-10-12 11:21:50 +0300

    User input

--- src/Main/StockPurchase.java
+++ src/Main/StockPurchase.java
@@ -1,6 +1,8 @@
 package Main;
 
+
 import java.util.Scanner;
+import finance.FinanceClass;
 
 /**
  * Created by kristi on 11/10/15.
@@ -8,9 +10,14 @@ import java.util.Scanner;
 public class StockPurchase {
     public static void main(String[] args) {
         Scanner user = new Scanner(System.in);
-        System.out.println("Palju maksab Tallinna Vee aktsia?");
-        int TallinnaVesiPrice = user.nextInt(); // Cast to int.
-        System.out.println("Aktsia hind on täna " + TallinnaVesiPrice);
+        System.out.println("Kui palju maksab aktsia, mida osta soovid?");
+        int stockPrice = (int) (user.nextDouble()*100); // Cast price to int in cents, only works with . not ,.
+
+        System.out.println("Mitu aktsiat soovid osta?");
+        int stockAmount = user.nextInt();
+
+        System.out.println("Aktsia hind on täna " + stockPrice + " senti." );
+        System.out.println("Soovid osta " + stockAmount + " aktsiat.");
 
     }
 }
---
+++ src/finance/FinanceClass.java
@@ -0,0 +1,8 @@
+package finance;
+
+/**
+ * Created by kristi on 11/10/15.
+ * All the math!
+ */
+public class FinanceClass {
+}
