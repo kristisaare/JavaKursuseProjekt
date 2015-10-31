14ac8316c06ede4168237cf368625aa58affd421
kristisaare <kristisaare@gmail.com>
2015-10-31 16:04:24 +0200

    Comments and progress

--- src/Main/StockPurchase.java
+++ src/Main/StockPurchase.java
@@ -13,7 +13,6 @@ import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 
 
 /**
@@ -32,26 +31,28 @@ public class StockPurchase extends Application {
         primaryStage.setTitle("Calculations");
 
         TextField askForPrice = new TextField();
-        askForPrice.setText("Enter stock price");
+        askForPrice.setText("Enter stock price (for example 12.34)");
 
         TextField askForQuantity = new TextField();
-        askForQuantity.setText("Enter number of stocks");
+        askForQuantity.setText("Enter number of stocks (for example 76)");
 
         button = new Button();
         button.setText("Calculate");
 
-        VBox window = new VBox();
+        VBox window = new VBox(); //Vertical box for all fields/buttons
         window.setSpacing(5);
         window.setAlignment(Pos.CENTER);
 
-
-        window.getChildren().add(askForPrice);
+        window.getChildren().add(askForPrice); //Builds the window
         window.getChildren().add(askForQuantity);
         window.getChildren().add(button);
 
-        Scene scene = new Scene(window, 400, 400);
+        Scene scene = new Scene(window, 400, 400); //Window properties
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        //double stockPrice = askForPrice.getText();//Figure out how to convert this
+        //int stockAmount = askForQuantity.getText(); // Figure out how to convert this
 
 
 
