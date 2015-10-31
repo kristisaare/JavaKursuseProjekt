500d650e1f572729e379daf4efab51aa4b0d4ac2
kristisaare <kristisaare@gmail.com>
2015-10-31 15:53:16 +0200

    window created

--- src/Main/StockPurchase.java
+++ src/Main/StockPurchase.java
@@ -3,21 +3,68 @@ package Main;
 
 import java.util.Scanner;
 import finance.FinanceClass;
+import finance.Validations;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
 
 /**
  * Created by kristi on 11/10/15.
  */
-public class StockPurchase {
+public class StockPurchase extends Application {
+
+    Button button;
+
     public static void main(String[] args) {
-        Scanner user = new Scanner(System.in);
-        System.out.println("Kui palju maksab aktsia, mida osta soovid?");
-        int stockPrice = (int) (user.nextDouble()*100); // Cast price to int in cents, only works with . not ,.
+        launch(args);
+    }
 
-        System.out.println("Mitu aktsiat soovid osta?");
-        int stockAmount = user.nextInt();
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Calculations");
 
-        System.out.println("Aktsia hind on täna " + stockPrice + " senti." );
-        System.out.println("Soovid osta " + stockAmount + " aktsiat.");
+        TextField askForPrice = new TextField();
+        askForPrice.setText("Enter stock price");
 
+        TextField askForQuantity = new TextField();
+        askForQuantity.setText("Enter number of stocks");
+
+        button = new Button();
+        button.setText("Calculate");
+
+        VBox window = new VBox();
+        window.setSpacing(5);
+        window.setAlignment(Pos.CENTER);
+
+
+        window.getChildren().add(askForPrice);
+        window.getChildren().add(askForQuantity);
+        window.getChildren().add(button);
+
+        Scene scene = new Scene(window, 400, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+
+
+
+        //Scanner user = new Scanner(System.in);
+        //System.out.println("Kui palju maksab aktsia, mida osta soovid?");
+        //int stockPrice = (int) (user.nextDouble()*100); // Cast price to int in cents, only works with . not ,.
+
+        //System.out.println("Mitu aktsiat soovid osta?");
+        //int stockAmount = user.nextInt();
+
+        //System.out.println("Aktsia hind on täna " + stockPrice + " senti." );
+        //System.out.println("Soovid osta " + stockAmount + " aktsiat.");
     }
 }
---
+++ src/finance/Validations.java
@@ -0,0 +1,7 @@
+package finance;
+
+/**
+ * Created by kristi on 12/10/15.
+ */
+public class Validations {
+}
